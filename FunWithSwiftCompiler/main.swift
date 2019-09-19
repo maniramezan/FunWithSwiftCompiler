ef4fb957eb5d767e96bc099157c104ee840ce537
Mani Ramezan <mani.ramezan@gmail.com>
2019-09-18 23:14:01 -0400

    Initial Commit

---
+++ FunWithSwiftCompiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FunWithSwiftCompiler
+//
+//  Created by Mani Ramezan on 9/18/19.
+//  Copyright © 2019 Mani Ramezan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
